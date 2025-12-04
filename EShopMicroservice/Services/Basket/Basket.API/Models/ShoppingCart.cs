058b396f5485fb6ec084daf705d5f7ff40c4c6ba
AmbroseSu <ambrose472263@gmail.com>
2025-12-04 15:07:17 +0700

    add_service_discount

--- EShopMicroservice/Services/Basket/Basket.API/Models/ShoppingCart.cs
+++ EShopMicroservice/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -5,6 +5,9 @@
         public string UserName { get; set; } = default!;
         public List<ShoppingCartItem> Items { get; set; } = new();
 
+        public decimal TotalPrice
+            => Items.Sum(item => item.Price * item.Quantity);
+
         public ShoppingCart(string username)
         {
             UserName = username;
--- EShopMicroservice/Services/Basket/Basket.API/Program.cs
+++ EShopMicroservice/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,13 @@
 using BuildingBlocks.Exceptions.Handler;
+using Discount.Grpc;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Application Services
 var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
@@ -26,12 +31,36 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
 
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+    {
+        options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+    })
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+        return handler;
+    });
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.MapHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
